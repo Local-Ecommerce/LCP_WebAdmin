3db5551cb19b3ef79633097f9f23c9a0c9f7551e
longg20 <longttse140019@fpt.edu.vn>
2022-02-24 14:13:49 +0700

    [24/2/2022 - LongTT] fix requestMethods

--- src/RequestMethod.jsx
+++ src/RequestMethod.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = "https://localhost:5001/api/";
+const BASE_URL = "https://localcommercialplatform-api.azurewebsites.net/api/";
 //"https://localhost:5001/api/";
 //"https://localcommercialplatform-api.azurewebsites.net/api/";
 
